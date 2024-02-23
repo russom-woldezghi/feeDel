06d096fe30768e67a5fd049aeabbe2739f57b709
Russom Woldezghi <russom.woldezghi@adhocteam.us>
2024-02-23 15:52:27 -0500

    org app into screens, components, nav, stub out data and styles in app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Navigation from './src/navigation';
+import { StyleSheet, ViewStyle } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+interface Item {
+  id: string;
+  title: string;
+}
+
+const DATA: Item[] = 
+[
+  { id: 'a0', title: 'ሀ' },
+  { id: 'a1', title: 'ሁ' },
+  { id: 'a2', title: 'ሂ' },
+  { id: 'a3', title: 'ሃ' },
+  { id: 'a4', title: 'ሄ' },
+  { id: 'a5', title: 'ህ' },
+  { id: 'a6', title: 'ሆ' },
+];
+
+interface Styles {
+  flexOne: ViewStyle;
+  item: ViewStyle;
+  itemImage: ViewStyle;
+  title: ViewStyle;
+  imageScreenTwo: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+  flexOne: { flex: 1 },
+  item: {
+    width: 90,
+    height: 100,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    overflow: 'hidden',
+  },
+  itemImage: {
+    width: '100%',
+    maxHeight: 100,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 55,
+  },
+  letter: {
+    fontSize: 100,
+    textAlign: 'center',
+  },
+  imageScreenTwo: {
+    width: '100%',
+    height: 200,
+  },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1, backgroundColor: 'red' }}>
+      <Navigation DATA={DATA} styles={styles} />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
